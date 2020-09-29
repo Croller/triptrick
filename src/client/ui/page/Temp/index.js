a379b253fb4240e69bb4c52bca1eeb2045c465ee
Alex <79104643015@yandex.ru>
2020-09-30 01:38:19 +0300

    edit Temp page component

--- src/client/ui/page/Temp/index.js
+++ src/client/ui/page/Temp/index.js
@@ -1,4 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { } from './styled';
 
-export const Temp = () => {};
+export const Temp = () => {
+  useEffect(() => {
+    window.prerenderReady = true;
+  }, []);
+
+  return (
+    <>
+    </>
+  );
+};
